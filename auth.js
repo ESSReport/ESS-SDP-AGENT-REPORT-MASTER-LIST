250bb8c28aee219621b1ab4da744aa582a972ab2
ESSReport <pacada.erl@gmail.com>
2026-02-18 10:20:26 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,33 @@
 const ADMIN_PIN = "11302024";
 const TL_PINS = {
   "TL-Emon": "820156",
-  "TL-LeoLiza": "758482",
-  "TL-Onemen": "754582",
-  "TL-RC": "872564"
+  "TL-LeoLiza": "872982",
+  "TL-Onemen": "872941",
+  "TL-RC": "872964",
+  "TL-Emon": "872994",
+  "TL-JAVED": "872741",
+  "TL-SHARIFLUX": "872264",
+  "TL-KING": "872914",
+  "TL-DAJAS": "872441",
+  "TL-KAHC": "872354",
+  "TL-RAYHEX": "872332",
+  "TL-MRLEE": "872994",
+  "TL-NUMM": "872621",
+  "TL-NOS": "872656",
+  "TL-LEWEJ": "872443",
+  "TL-SEXELA": "872882",
+  "TL-INOR": "872431",
+  "TL-RAIRA": "872557",
+  "TL-NOMIL": "872229",
+  "TL-LASAR": "872659",
+  "TL-KCAJ": "872454",
+  "TL-URMAK": "872323",
+  "TL-VORUOS": "872922",
+  "TL-NAJUS": "872375",
+  "TL-DEAN": "872897",
+  "TL-ALEXVENUS": "872326",
+  "TL-YOB": "872919",
+  "TL-DARAZ": "872439"
 };
 
 // -------------------------
@@ -44,6 +68,7 @@ window.checkTLAccess = async function(tlName) {
   if (!tlName) throw new Error("Team Leader not specified");
   return await window.requireTeamLeader(tlName);
 };
+
 
 
 
